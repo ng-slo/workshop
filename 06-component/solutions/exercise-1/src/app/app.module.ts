788caa4574f16d15ba3a6e61e2edc17cce1b7c59
NejcZdovc <nejc.zdovc@3zsistemi.si>
2017-04-23 20:54:27 +0200

    Excercises for workshop 6

---
+++ 06-component/solutions/exercise-1/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+
+import { AppComponent } from './app.component';
+import { FamilyTreeComponent } from './family-tree/family-tree.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    FamilyTreeComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
